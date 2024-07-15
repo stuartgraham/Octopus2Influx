4d8f334352e21069f7277fa29a470cf5e0412e91
Stuart Graham <stuart@stuart-graham.com>
2024-07-15 21:22:26 +0100

    feat: default LIVE_CONN

--- main.py
+++ main.py
@@ -12,20 +12,19 @@ from urllib3 import Retry
 import schedule
 
 # GLOBALS
-CRON_MODE = bool(os.environ.get('CORN_MODE', False))
-INFLUX_VERSION = int(os.environ.get("INFLUX_VERSION", 2))
-LIVE_CONN = bool(os.environ['LIVE_CONN'])
+CRON_MODE = bool(os.environ.get("CRON_MODE", False))
+LIVE_CONN = bool(os.environ.get("LIVE_CONN", True))
 INFLUX_HOST = os.environ.get("INFLUX_HOST", "")
 INFLUX_HOST_PORT = int(os.environ.get("INFLUX_HOST_PORT", 8086))
 INFLUX_BUCKET = os.environ.get("INFLUX_BUCKET", "")
 INFLUX_TOKEN = os.environ.get("INFLUX_TOKEN", "")
 INFLUX_ORG = os.environ.get("INFLUX_ORG", "-")
-APIKEY = os.environ['APIKEY']
-ELECMPAN = os.environ['ELECMPAN']
-ELECSERIAL = os.environ['ELECSERIAL']
-GASMPAN = os.environ['GASMPAN']
-GASSERIAL = os.environ['GASSERIAL']
-RUNMINS =  int(os.environ.get('RUNMINS', 60))
+APIKEY = os.environ["APIKEY"]
+ELECMPAN = os.environ["ELECMPAN"]
+ELECSERIAL = os.environ["ELECSERIAL"]
+GASMPAN = os.environ["GASMPAN"]
+GASSERIAL = os.environ["GASSERIAL"]
+RUNMINS =  int(os.environ.get("RUNMINS", 60))
 LOGGING = bool(os.environ.get("LOGGING", False))
 
 # Logging
@@ -35,17 +34,17 @@ if not LOGGING:
 JSON_OUTPUT = "output.json"
 
 def construct_url(*args):
-    if args[0] == 'electricty':
-        url = f'https://api.octopus.energy/v1/electricity-meter-points/{ELECMPAN}/meters/{ELECSERIAL}/consumption/'
-    if args[0] == 'gas':
-        url = f'https://api.octopus.energy/v1/gas-meter-points/{GASMPAN}/meters/{GASSERIAL}/consumption/'
+    if args[0] == "electricty":
+        url = f"https://api.octopus.energy/v1/electricity-meter-points/{ELECMPAN}/meters/{ELECSERIAL}/consumption/"
+    if args[0] == "gas":
+        url = f"https://api.octopus.energy/v1/gas-meter-points/{GASMPAN}/meters/{GASSERIAL}/consumption/"
     return url
 
 # Get json data
 def get_json(*args): 
-    resp = requests.get(args[0], auth = HTTPBasicAuth(APIKEY,''))
+    resp = requests.get(args[0], auth = HTTPBasicAuth(APIKEY,""))
     payload_data = resp.json()
-    with open(JSON_OUTPUT, 'w') as outfile:
+    with open(JSON_OUTPUT, "w") as outfile:
         json.dump(payload_data, outfile)
 
 def get_saved_data(*args):
@@ -71,39 +70,39 @@ def write_to_influx(data_payload):
         
     data_points = len(data_payload)
     print(f"SUCCESS: {data_points} data points written to InfluxDB")
-    print('#'*30)
+    print("#"*30)
     client.close()
 
 
 def sort_json(working_data, energy_type):
-    if energy_type == 'electricty':
-        unit_type = 'kWh'
-    if energy_type == 'gas':
-        unit_type = 'm3'
+    if energy_type == "electricty":
+        unit_type = "kWh"
+    if energy_type == "gas":
+        unit_type = "m3"
     
-    for result in working_data['results']:
-        base_dict = {'measurement' : energy_type, 'tags' : {'name': energy_type}}
-        base_dict.update({'time': result['interval_end']})
-        fields_data = {unit_type : float(result['consumption'])}
-        base_dict.update({'fields' : fields_data})
+    for result in working_data["results"]:
+        base_dict = {"measurement" : energy_type, "tags" : {"name": energy_type}}
+        base_dict.update({"time": result["interval_end"]})
+        fields_data = {unit_type : float(result["consumption"])}
+        base_dict.update({"fields" : fields_data})
         data_payload = [base_dict]
         print("SUBMIT:" + str(data_payload))
-        print('#'*30)
+        print("#"*30)
         write_to_influx(data_payload)
 
 
 def do_it(*args):
-    url = construct_url('electricty')
+    url = construct_url("electricty")
     working_data = get_saved_data(url)
-    sort_json(working_data, 'electricty')
+    sort_json(working_data, "electricty")
 
-    url = construct_url('gas')
+    url = construct_url("gas")
     working_data = get_saved_data(url)
-    sort_json(working_data, 'gas')
+    sort_json(working_data, "gas")
 
 
 def main():
-    ''' Main entry point of the app '''
+    """ Main entry point of the app """
     # Cron mode runs once and signals an exit code
     if CRON_MODE:
         try:
@@ -124,6 +123,6 @@ def main():
             schedule.run_pending()
             time.sleep(1)
 
-if __name__ == '__main__':
-    ''' This is executed when run from the command line '''
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
     main()
